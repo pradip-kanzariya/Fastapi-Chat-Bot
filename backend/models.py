ce850bfd553b94b3432d709f4a9c06c985207644
pradip-kanzariya <pradip.kanzariya@armakuni.com>
2025-08-28 11:59:38 +0530

    Changes in model: add new table users and add foreigneky to chat_history table

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,16 @@
-from sqlalchemy import Column, Integer, Text, DateTime, String
+from sqlalchemy import Column, Integer, Text, DateTime, String, ForeignKey
 from datetime import datetime
 from backend.db import Base
+
+class Users(Base):
+
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, nullable=False)
+    user_email = Column(String, nullable=False, unique=True)
+    user_password = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.now)
 
 class ChatHistory(Base):
 
@@ -8,8 +18,10 @@ class ChatHistory(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     session_id = Column(String, index=True, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"))
     user_question = Column(Text, nullable=True)
     model_answer = Column(Text, nullable=True)
     created_at = Column(DateTime, default=datetime.now)
     file_data = Column(Text, nullable=True)
     embedding = Column(Text, nullable=True)
+
